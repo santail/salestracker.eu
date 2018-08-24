88ddb65f0319659ccda01e201df1b816a34f496a
Nikolai Muhhin <nimuhhin@microsoft.com>
2018-08-24 21:10:37 +0300

    Turn Finder to TypeScript.

--- salestracker.eu-finder/src/index.ts
+++ salestracker.eu-finder/src/index.ts
@@ -1,11 +1,25 @@
+var SessionFactory = require("../lib/services/SessionFactory");
+
 var start = Date.now();
+var dbConnection = SessionFactory.getDbConnection();
 
-console.log("starting timer...");
-// expected output: starting timer...
+setInterval(function () {
+    var now = Date.now();
 
-setTimeout(function() {
-  var millis = Date.now() - start;
+    dbConnection.wishes.findAndModify({
+        query: { 
+            last_time_checked: { $lt: now } 
+        },
+        update: { 
+            $set: { last_time_checked: now } 
+        },
+        new: true
+    }, function (err: any, doc: any) {
+        if (err) {
+            console.error(err);
+        }
 
-  console.log("seconds elapsed = " + Math.floor(millis/1000));
-  // expected output : seconds elapsed = 2
+        console.log(doc)
+    });
+
 }, 2000);
