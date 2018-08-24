133aa0c9a28bde050b90162f52eabc352f916b2d
Nikolai Muhhin <nimuhhin@microsoft.com>
2018-08-24 21:07:24 +0300

    Simplify session factory

--- salestracker.eu-harvester/lib/services/SessionFactory.js
+++ salestracker.eu-harvester/lib/services/SessionFactory.js
@@ -6,19 +6,19 @@ var mongojs = require('mongojs');
 var LOG = require("./Logger");
 
 let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://mongo:27017/salestracker-dev';
-let REDIS_ADDR = process.env.REDIS_ADDR || '127.0.0.1:6379';
+let REDIS_ADDR = process.env.REDIS_ADDR || 'redis://127.0.0.1:6379';
 
 function Sessionfactory() {
   this.db = mongojs(MONGODB_URI, ["offers", "sites", "wishes"]);
 
   this.worker = kue.createQueue({
-    redis: `redis://${REDIS_ADDR}`,
+    redis: REDIS_ADDR,
     restore: true,
     worker: true
   });
 
   this.scheduler = kue.createQueue({
-    redis: `redis://${REDIS_ADDR}`,
+    redis: REDIS_ADDR,
     restore: true,
     worker: false
   });
@@ -36,10 +36,4 @@ Sessionfactory.prototype.getSchedulerConnection = function () {
   return this.scheduler;
 };
 
-var sessionFactory = new Sessionfactory();
-
-module.exports = {
-  getDbConnection: sessionFactory.getDbConnection.bind(sessionFactory),
-  getQueueConnection: sessionFactory.getQueueConnection.bind(sessionFactory),
-  getSchedulerConnection: sessionFactory.getSchedulerConnection.bind(sessionFactory)
-};
+module.exports = new Sessionfactory();
