8ea8360e17a2e5c25a9be5b733dc2b011ffb1d75
Nikolai Muhhin <nimuhhin@microsoft.com>
2018-08-24 19:08:24 +0300

    Add seeding service

---
+++ salestracker.eu-seeds/data/wishes/data.js
@@ -0,0 +1,18 @@
+const wishes = [
+  {
+    "contains": "burger",
+    "email": "nikolai.muhhin@gmail.com",
+    "phone": "+37253003125",
+    "active": true,
+    "last_time_checked": Date.now()
+  },
+  {
+    "contains": "Lavendel Spa",
+    "email": "nikolai.muhhin@gmail.com",
+    "phone": "+37253003125",
+    "active": true,
+    "last_time_checked": Date.now()
+  }
+];
+
+module.exports = wishes;
