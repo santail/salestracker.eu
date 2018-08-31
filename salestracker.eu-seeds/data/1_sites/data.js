f4e093ba53aeaa341d983ad3bbeceadf5c71c1ca
Nikolai Muhhin <nimuhhin@microsoft.com>
2018-08-31 14:57:04 +0300

    Add Site and Wish seeds

---
+++ salestracker.eu-seeds/data/1_sites/data.js
@@ -0,0 +1,11 @@
+const sites = [
+  {
+    "name": "Minuvalik.ee",
+    "url": "www.minuvalik.ee",
+    "active": true,
+    "created": Date.now(),
+    "modified": Date.now()
+  }
+];
+
+module.exports = sites;
