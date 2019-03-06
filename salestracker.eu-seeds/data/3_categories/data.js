16fec705f86eb37944aa252edbec49ec728007e8
Nikolai Muhhin <nimuhhin@microsoft.com>
2019-03-06 16:49:06 +0200

    Add categories seeds

---
+++ salestracker.eu-seeds/data/3_categories/data.js
@@ -0,0 +1,21 @@
+const categories = [{
+    "category": "alcohol",
+    "tags": ["valge vein", "punane vein", "roosa vein", "Caribbean dark", "Caribbean white", "Gin Gordons", "Gin Tanqueray", "Negrita Dark Rhum", "Negrita White Rhum", "Sauvignon", "Bordeaux", "Pinot", "Mojito", "Brut Rose", "Scottish Leader", "Martini Bianco", "Johnnie Walker", "Captain Morgan", "Gato Negro", "Stolichnaya", "Chenin Blanc", "Bacardi", "Beefeater", "Fresita", "Zacapa Negra", "Veuve du Vernay", "St. Remy", "Moet & Chandon", "Shiraz", "A.LE COQ", "Vana Tallinn", "Viru Valge", "Riesling", "Cabernet", "Chardonnay", "Merlot", "Hennessy", "Cognac", "Liviko Liköör", "Caribba Blanco", "Freixenet Cordon", "Caribba Negro", "Larsen V.S", "Cinzano", "Saaremaa Vodka", "Russian Standart", "Baileys", "Calvados Coquerel", "Ballantine's", "Scotch Whisky", "Pinot Grigio", "Jack Daniel's", "Jim Beam", "Jägermeister", "Leffe Blonde", "Tullamore Dew", "Törley", "Krusovice", "Pipra Naps", "Gran Reserva", "Nederburg Winemasters", "Somersby Orchard", "J. P. Chenet", "Metsis Handcrafted", "Prosecco", "Cointreau", "Veuve Clicquot", "St.Remy", "Martell V.S", "J.P.Chenet", "Ibis brändi X.O.", "Sierra Tequila", "Russian Standard", "Beluga Noble", "Carlsberg ", "Aramis V", "Gran Castillo", "Old Tbilisi", "Rigol Cava", "Torres", "Napoleon VS", "Rigol Cava", "Torres", "Havana Club", "Angostura Rum Dark", "Y2K Premium", "Zacapa Negra"]
+}, {
+    "category": "cosmetics",
+    "tags": ["Always Night", "Always Ultra", "Himalaya", "Garnier", "Vuokkoset", "Lirene Mineral", "L'Oreal Paris", "Loreal Color", "Loreal Eye", "Loreal Glam", "Loreal Infallible", "Loreal Men", "Loreal Revitalift", "Loreal Telescopic", "Loreal Unlimited", "Nivea Men", "Nivea Mild", "Nivea Sensitive", "VEET Debut", "Gillette", "ORAL-B", "COLGATE", "Maybelline", "Maybellin", "Max Factor", "Schwarzkopf", "SENSODYNE", "Tattoobrow", "LISTERINE"]
+}, {
+    "category": "children",
+    "tags": ["Pampers", "HUGGIES"]
+}, {
+    "category": "toys",
+    "tags": ["LEGO FRIENDS", "TRANSFORMERS", "MONOPOLY", "LITTLEST PET SHOP", "MY LITTLE PONY", "L.O.L", "BRUDER", "GERARDOS TOYS", "GERARDO´S TOYS", "LEGO"]
+}, {
+    "category": "fashion",
+    "tags": []
+}, {
+    "category": "pets",
+    "tags": ["KITEKAT", "PURINA ONE"]
+}];
+
+module.exports = categories;
