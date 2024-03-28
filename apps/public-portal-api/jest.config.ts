b38d1ac43c6138cc3d307d6a79df5706938d8cf0
Nikolai Muhhin <nikolai.muhhin@funderbeam.com>
2024-03-28 16:20:49 +0200

    Add public portal API project

---
+++ apps/public-portal-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'public-portal-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/public-portal-api',
+};
