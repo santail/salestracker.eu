0442ffc98232970c2ec162d31892009a4bb783fb
Nikolai Muhhin <nikolai.muhhin@funderbeam.com>
2024-08-19 16:20:15 +0300

    Code cleanup

--- apps/public-portal-frontend/src/app/app.spec.tsx
+++ apps/public-portal-frontend/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome salestracker.eu/gi)).toBeTruthy();
+    expect(getByText(/Welcome salestracker.eu/i)).toBeTruthy();
   });
 });
